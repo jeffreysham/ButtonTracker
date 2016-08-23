bdcdc89636263ab967ac596a5f922df92dd97d37
grawson <grawson2@jhu.edu>
2016-08-23 18:21:24 -0400

    Initial Commit

---
+++ ButtonTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ButtonTracker
+//
+//  Created by Gavi Rawson on 8/23/16.
+//  Copyright © 2016 Corrie Health. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
